fb2b3618313d18caa76020226230f73855ac7b96
Florent GONON <florent.gonon33@gmail.com>
2019-11-13 15:34:55 +0100

    Yelp MVP DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: " Tripletta ", address: "150 rue Sainte Catherine, Bordeaux ", phone_number: "0356748372", category: "italian")
+Restaurant.create(name: " Burdiga Pizza ", address: "3 Place Amédée Larrieu, Bordeaux ", phone_number: "0656518390", category: "italian")
+Restaurant.create(name: " Mélodie ", address: "50 rue des faussets, Bordeaux ", phone_number: "0789547624", category: "french")
+Restaurant.create(name: " Okinii ", address: "26 cours Edouard Vaillant, Bordeaux ", phone_number: "0556734256", category: "japanese")
+Restaurant.create(name: " French Burger ", address: "75 quais des Chartrons, Bordeaux ", phone_number: "0297879862", category: "french")
+
+Restaurant.all.each do |r|
+  puts "Created #{r.name} "
+end
